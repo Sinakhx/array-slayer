8a19058abc71d76ed0d0252229251d063238460a
Sina Khodabandehloo <dr.sina121@gmail.com>
2021-07-18 03:51:47 +0430

    build

--- dist/fp/clearByValues.js
+++ dist/fp/clearByValues.js
@@ -1,2 +1,2 @@
-var i=(n=[],...s)=>{let t=[...s].map(e=>JSON.stringify(e)),a=new Set([...t]);return n.map(e=>a.has(JSON.stringify(e))?void 0:e)};export{i as clearByValues};
+var i=(n=[],...s)=>{let t=s.map(e=>JSON.stringify(e)),a=new Set(t);return n.map(e=>a.has(JSON.stringify(e))?void 0:e)};export{i as clearByValues};
 //# sourceMappingURL=clearByValues.js.map
--- dist/fp/delete.js
+++
@@ -1,2 +0,0 @@
-var i=(t=[],...n)=>{let s=new Set([...n]);return t.filter((r,e)=>!s.has(e))},l=(t=[],...n)=>{let s=[...n].map(e=>JSON.stringify(e)),r=new Set([...s]);return t.filter(e=>!r.has(JSON.stringify(e)))};export{i as deleteByIndexes,l as deleteByValues};
-//# sourceMappingURL=delete.js.map
