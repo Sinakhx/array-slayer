edaddb4cb23ef1d0c3cfe1ed1aa6e967fdc06b19
Sina Khodabandehloo <dr.sina121@gmail.com>
2021-07-18 04:10:27 +0430

    build

---
+++ dist/fp/deleteByIndexes.js
@@ -0,0 +1,2 @@
+var d=(e=[],...t)=>{let n=new Set(t);return e.filter((r,s)=>!n.has(s))};export{d as deleteByIndexes};
+//# sourceMappingURL=deleteByIndexes.js.map
---
+++ dist/fp/deleteByValues.js
@@ -0,0 +1,2 @@
+var r=(t=[],...s)=>{let i=s.map(e=>JSON.stringify(e)),n=new Set(i);return t.filter(e=>!n.has(JSON.stringify(e)))};export{r as deleteByValues};
+//# sourceMappingURL=deleteByValues.js.map
